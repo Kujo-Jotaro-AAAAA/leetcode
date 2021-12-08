4f57e47a36ce6193d41a3748d9fe81a00013dfe9
zjotaro <zzc5464@foxmail.com>
2021-12-08 14:54:33 +0800

    三数字之和

--- 数组/三数之和(未解决).js
+++ 数组/三数之和(未解决).js
@@ -16,10 +16,40 @@
 输出：[]
 */
 /**
+ * 解答:
+ * 1. 将数组按顺序排列
+ * 2. 定义两个指针: left = i + 1, right = len - 1
+ * 3. 如果当前位置的值 + left指针的值 + right指针的值 为0 , 则push进结果
+ * 4. 结果 < 0, 左指针++, > 0 右指针 --
+ * 5. 处理重复数字的情况
+ * 6. 已排序且当前的值已经>0, 则跳出不处理
+ * 7. 数组的第二位开始, 如果跟前一位值相同, 则结束此次循环，执行下一次循环
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-	if(nums.length < 3) return []
-	
+var threeSum = function (nums) {
+  if (!Array.isArray(nums) || nums.length < 3) return [];
+  nums.sort();
+  let ans = [];
+  const len = nums.length;
+  for (let i = 0; i < len; i++) {
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    let left = i + 1,
+      right = len - 1;
+    while (left < right) {
+      let sum = nums[i] + nums[left] + nums[right];
+      if (sum === 0) {
+        ans.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1]) right--;
+        left++;
+        right--;
+      } else if (sum < 0) left++;
+      else if (sum > 0) right--;
+    }
+  }
+  return ans;
 };
+const nums = [0, 0, 0, 0, 0, 0];
+// const nums = [-1, 0, 1, 2, -1, -4];
+threeSum(nums);
