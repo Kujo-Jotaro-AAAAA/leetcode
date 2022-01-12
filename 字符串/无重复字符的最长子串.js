5276b6f6742b3b3cb1d312fe65ddd1ba31bc85b8
zjotaro <zzc5464@foxmail.com>
2022-01-12 11:20:29 +0800

    feat: 无重复

--- 字符串/无重复字符的最长子串.js
+++ 字符串/无重复字符的最长子串.js
@@ -3,7 +3,7 @@
   输出: 3 
   解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  */
-var s = "bbbbb";
+var s = "abcabcbb";
 var lengthOfLongestSubstring = (s) => {
   if (!s) return 0;
   var ans = 0,
@@ -14,14 +14,24 @@ var lengthOfLongestSubstring = (s) => {
       strSet.delete(s.charAt(i - 1));
     }
     while (
+      // 在字符长度边界内, 且set里面不存在此字符时不停的循环
       windowIndex + 1 < s.length &&
       !strSet.has(s.charAt(windowIndex + 1))
     ) {
+      // 将他添加到set并windowIndex计数+1
       strSet.add(s.charAt(windowIndex + 1));
       ++windowIndex;
     }
+    // 比对 windowIndex计数 跟当前遍历下标的距离
     ans = Math.max(ans, windowIndex - i + 1);
   }
   return ans;
 };
 lengthOfLongestSubstring(s);
+/**
+ * "abcabcbb"
+ * 就像手写记录, 每次循环拿到最长的不重复字符串, 下次循环去掉当前循环前一个字符
+ * set: abc -> bca -> cab -> abc -> cb -> b -> b
+ * windowIndex + 1: 3 -> 3 -> 3 -> 3 -> 2 -> 1 -> 1
+ * ans: 3 -> 3 -> 3 -> 3 -> 3 -> 3 -> 3 -> 3 -> 3
+ */
